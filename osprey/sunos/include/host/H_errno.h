ae6d17babb966cfc64ed18b458bfda046651699e
zoryj <zoryj@9f6548e3-d00b-0410-a34f-e49d3b37daf0>
2002-08-05 14:24:47 +0000

    host specific includes
    git-svn-id: https://codex.cro.st.com/svnroot/open64/trunk@959 9f6548e3-d00b-0410-a34f-e49d3b37daf0

---
+++ osprey/sunos/include/host/H_errno.h
@@ -0,0 +1,10 @@
+
+#ifndef H_ERRNO_H
+#define H_ERRNO_H
+
+#include <errno.h>
+
+extern char* sys_errlist[];
+extern int sys_nerr;
+
+#endif /* H_ERRNO_H */
---
+++ osprey/sunos/include/host/H_limits.h
@@ -0,0 +1,11 @@
+
+#ifndef H_LIMITS_H
+#define H_LIMITS_H
+
+#include <limits.h>
+
+#define LONGLONG_MAX LLONG_MAX
+#define LONGLONG_MIN LLONG_MIN
+#define ULONGLONG_MAX ULLONG_MAX
+
+#endif /* H_LIMITS_H */
