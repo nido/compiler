fc11624bacf6a541e7bfd60296d7b1049575828f
bruel <bruel@9f6548e3-d00b-0410-a34f-e49d3b37daf0>
2002-10-29 11:04:24 +0000

    no Advanced_loads for st100
    git-svn-id: https://codex.cro.st.com/svnroot/open64/trunk@1468 9f6548e3-d00b-0410-a34f-e49d3b37daf0

--- osprey/targinfo/st100/cg/targ_op.cxx
+++ osprey/targinfo/st100/cg/targ_op.cxx
@@ -77,8 +77,10 @@ OP_Is_Counted_Loop(OP *op)
 BOOL
 OP_Is_Advanced_Load( OP *memop )
 {
+#if 0
   if (!OP_load(memop)) return FALSE;
   if (TOP_is_dismissible(OP_code(memop))) return TRUE;
+#endif
 
   return FALSE;
 }
