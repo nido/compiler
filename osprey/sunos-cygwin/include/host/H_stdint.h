5386abc7a4693a165c7d96201eedc859bc2e891d
lyon <lyon@9f6548e3-d00b-0410-a34f-e49d3b37daf0>
2003-03-24 13:08:54 +0000

    [CL] MERGE Fix for cygwin build with LAO
    git-svn-id: https://codex.cro.st.com/svnroot/open64/trunk@2307 9f6548e3-d00b-0410-a34f-e49d3b37daf0

--- osprey/sunos-cygwin/include/host/H_stdint.h
+++ osprey/sunos-cygwin/include/host/H_stdint.h
@@ -10,4 +10,7 @@ typedef u_int16_t uint16_t;
 typedef u_int32_t uint32_t;
 typedef u_int64_t uint64_t;
 
+typedef signed long     intptr_t;
+typedef unsigned long   uintptr_t;
+
 #endif /* H_STDINT_H */
