e7185b02bff0eb16ab9a3f0102f7045e6dcfae42
bidaultt <bidaultt@9f6548e3-d00b-0410-a34f-e49d3b37daf0>
2003-12-03 15:30:20 +0000

    [TB] When compiling with st200cc include string.h instead
    git-svn-id: https://codex.cro.st.com/svnroot/open64/trunk@3707 9f6548e3-d00b-0410-a34f-e49d3b37daf0

--- osprey/sunos/include/host/H_bstring.h
+++ osprey/sunos/include/host/H_bstring.h
@@ -2,6 +2,10 @@
 #ifndef H_BSTRING_H
 #define H_BSTRING_H
 
+#if !defined(__open64_) && !defined(__ST200)
 #include <strings.h>
-
+#else
+/* [TB] When compiling with st200cc include string.h instead */
+#include <string.h>
+#endif
 #endif /* H_BSTRING_H */
