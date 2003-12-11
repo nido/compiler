90e96b6456eae5e75144cdf82131da9ec2598600
ferriere <ferriere@9f6548e3-d00b-0410-a34f-e49d3b37daf0>
2003-12-11 09:57:13 +0000

    Do not unroll remainder loop at -Os
    git-svn-id: https://codex.cro.st.com/svnroot/open64/trunk@3754 9f6548e3-d00b-0410-a34f-e49d3b37daf0

--- osprey/lai/cgdriver.cxx
+++ osprey/lai/cgdriver.cxx
@@ -1087,6 +1087,9 @@ Configure_CG_Options(void)
     }
   }
 
+  if (Opt_Level == 2 && Opt_ForSize)
+    CG_LOOP_unroll_remainder_fully = FALSE;
+
   if (!CG_LOOP_unroll_multi_bb_overridden)
     CG_LOOP_unroll_multi_bb = FALSE;
 
