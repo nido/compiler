d6c4dca04725226ad92190c9f6198ff66d454c0e
monat <monat@9f6548e3-d00b-0410-a34f-e49d3b37daf0>
2004-03-24 07:11:47 +0000

    [CM] MERGE Handle st231, remove st221, use predicates.
    git-svn-id: https://codex.cro.st.com/svnroot/open64/trunk@4180 9f6548e3-d00b-0410-a34f-e49d3b37daf0

--- osprey/targinfo/st200/config/config_cache_targ.cxx
+++ osprey/targinfo/st200/config/config_cache_targ.cxx
@@ -62,9 +62,9 @@ static const char source_file[] = __FILE__;
 
 void MHD::Initialize()
 {
-  if (Target == TARGET_st220 ||
-      Target == TARGET_st221 ||
-      Target == TARGET_st230) {
+  if ( Is_Target_st220() ||
+       Is_Target_st230() ||
+       Is_Target_st231() ) {
 
     Non_Blocking_Loads      = FALSE;
     Loop_Overhead_Base      = 6;
