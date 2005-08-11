a6aa1abcefa589ea090df14a19860ccb62df871c
bd3 <bd3@9f6548e3-d00b-0410-a34f-e49d3b37daf0>
2005-08-11 14:06:49 +0000

    Regenerate lao interface files.
    git-svn-id: https://codex.cro.st.com/svnroot/open64/trunk@6714 9f6548e3-d00b-0410-a34f-e49d3b37daf0

--- osprey/lai/lao/lao_stub.h
+++ osprey/lai/lao/lao_stub.h
@@ -1,73 +1,8 @@
-#define __GXX_ABI_VERSION 102
-#define __SIZE_TYPE__ unsigned int
-#define __PTRDIFF_TYPE__ int
-#define __WCHAR_TYPE__ long int
-#define __WINT_TYPE__ long int
-#define __SCHAR_MAX__ 127
-#define __SHRT_MAX__ 32767
-#define __INT_MAX__ 2147483647
-#define __LONG_MAX__ 2147483647L
-#define __LONG_LONG_MAX__ 9223372036854775807LL
-#define __WCHAR_MAX__ 2147483647
-#define __CHAR_BIT__ 8
-#define __FLT_EVAL_METHOD__ 0
-#define __FLT_RADIX__ 2
-#define __FLT_MANT_DIG__ 24
-#define __FLT_DIG__ 6
-#define __FLT_MIN_EXP__ (-125)
-#define __FLT_MIN_10_EXP__ (-37)
-#define __FLT_MAX_EXP__ 128
-#define __FLT_MAX_10_EXP__ 38
-#define __FLT_MAX__ 3.40282347e+38F
-#define __FLT_MIN__ 1.17549435e-38F
-#define __FLT_EPSILON__ 1.19209290e-7F
-#define __FLT_DENORM_MIN__ 1.40129846e-45F
-#define __DBL_MANT_DIG__ 53
-#define __DBL_DIG__ 15
-#define __DBL_MIN_EXP__ (-1021)
-#define __DBL_MIN_10_EXP__ (-307)
-#define __DBL_MAX_EXP__ 1024
-#define __DBL_MAX_10_EXP__ 308
-#define __DBL_MAX__ 1.7976931348623157e+308
-#define __DBL_MIN__ 2.2250738585072014e-308
-#define __DBL_EPSILON__ 2.2204460492503131e-16
-#define __DBL_DENORM_MIN__ 4.9406564584124654e-324
-#define __LDBL_MANT_DIG__ 113
-#define __LDBL_DIG__ 33
-#define __LDBL_MIN_EXP__ (-16381)
-#define __LDBL_MIN_10_EXP__ (-4931)
-#define __LDBL_MAX_EXP__ 16384
-#define __LDBL_MAX_10_EXP__ 4932
-#define __DECIMAL_DIG__ 36
-#define __LDBL_MAX__ 1.18973149535723176508575932662800702e+4932L
-#define __LDBL_MIN__ 3.36210314311209350626267781732175260e-4932L
-#define __LDBL_EPSILON__ 1.92592994438723585305597794258492732e-34L
-#define __LDBL_DENORM_MIN__ 6.47517511943802511092443895822764655e-4966L
-#define __REGISTER_PREFIX__ 
-#define __USER_LABEL_PREFIX__ 
-#define __VERSION__ "3.3.3"
-#define __STDC_HOSTED__ 1
-#define __NO_INLINE__ 1
-#define __FINITE_MATH_ONLY__ 0
-#define __unix 1
-#define __unix__ 1
+
+
+#define i386 1
+#define linux 1
 #define unix 1
-#define __sun 1
-#define __sun__ 1
-#define sun 1
-#define __svr4__ 1
-#define __SVR4 1
-#define __PRAGMA_REDEFINE_EXTNAME 1
-
-
-#define __GNUC__ 3
-#define __GNUC_MINOR__ 3
-#define __GNUC_PATCHLEVEL__ 3
-#define sparc 1
-#define __sparc__ 1
-#define __sparc 1
-#define __GCC_NEW_VARARGS__ 1
-
 
 /*
   ConfigureRegionType -- enumerates the ConfigureRegionType types.
