3b12c21567420f2d619a6183ad55642dd177c684
vincent_lorquet <vincent_lorquet@9f6548e3-d00b-0410-a34f-e49d3b37daf0>
2007-08-24 09:31:15 +0000

    ------------------------------------------------------                SVNBRANCH CHANGESET ------------------------------------------------------ 	Date  :    Fri Aug 24 11:29:46 CEST 2007 	Author:    lv21 	Parent:    https://codex.cro.st.com/svnroot/open64/trunk 	Branch:    https://codex.cro.st.com/svnroot/open64/branches/fixes/codex-31205
    Description:
    ------------
    	* Delivered fix for bug #31205 after rebase from trunk at rev #13059, with approval
    	  in branch #31363 
    
    
    Modified Files:
    ---------------
    	M      osprey/driver/opt_actions.c
    
    
    
    
    git-svn-id: https://codex.cro.st.com/svnroot/open64/trunk@13091 9f6548e3-d00b-0410-a34f-e49d3b37daf0

--- osprey/driver/opt_actions.c
+++ osprey/driver/opt_actions.c
@@ -847,31 +847,36 @@ Check_Target ( void )
     
   /* -1 = default ; 0 : none 1: hwlooponly 2; jrgtudeconly 3: all  */
   int core_has_hwloop = (corecfg & 0x300000) != 0;
-  /* select hwloop_mapping depending on olevel, osize and hwloop
-     option */
-  if (!core_has_hwloop) /* configuration without HW Loop */
-    {
-    if (hwloop_mapping==hwloop_all)
-  	   warning("HW loop mapping deactivated due to core configuration");
-    if (olevel>0)
-      hwloop_mapping = jrgtudeconly;
-    else
-      hwloop_mapping = hwloop_none;
+  /* select hwloop_mapping depending on olevel, osize hwloop option */
+  /* and core configuration                                         */
+
+  if (!core_has_hwloop) { /* configuration without HW Loops */ 
+
+    /* a warning is emmited if the user specified -Mhwloop=all */
+    if (hwloop_mapping==hwloop_all) {
+      warning("HW loop mapping deactivated due to core configuration - considers default instead");
+      hwloop_mapping=hwloop_default; /* mapping as default instead */
     }
-  else if (hwloop_mapping==hwloop_default)
-    {
+
+    /* if default chosen, mapping is set according to optimization level*/
+    if (hwloop_mapping==hwloop_default) {
       if (olevel>0)
-        {
-          if (osize>0)
-            hwloop_mapping= jrgtudeconly;
-          else
-            hwloop_mapping= hwloop_all;
-        }
+        hwloop_mapping = jrgtudeconly;
       else
-        {
-          hwloop_mapping = hwloop_none;
-        }
+        hwloop_mapping = hwloop_none;
     }
+  } else if (hwloop_mapping==hwloop_default) { /* configuration with HW Loops & default */
+
+    /* as default chosen, mapping is set according to optimization level*/
+    if (olevel>0) {
+        if (osize>0)
+          hwloop_mapping= jrgtudeconly;
+        else
+          hwloop_mapping= hwloop_all;
+    } else {
+        hwloop_mapping = hwloop_none;
+    }
+  }
 
   add_stxp70_int_option("-TARG:activate_hwloop=%d", hwloop_mapping, P_be);
   add_stxp70_int_option("-TARG:core_has_hwloop=%d", core_has_hwloop?  1 : 0, P_be);
