7510f220996f15d8e028c662032e70860c3f98f1
ferriere <ferriere@9f6548e3-d00b-0410-a34f-e49d3b37daf0>
2010-02-24 09:18:55 +0000

    ------------------------------------------------------                SVNBRANCH CHANGESET ------------------------------------------------------ 	Date  :    Wed Feb 24 10:15:58 CET 2010 	Author:    ferriere 	Parent:    https://codex.cro.st.com/svnroot/open64/trunk 	Branch:    https://codex.cro.st.com/svnroot/open64/branches/fixes/codex-90977
    Description:
    ------------
    	* [trunk] Fix for bug #90977: Incorrect unrolling information after automod transformation.
    	Deliver fixes/codex-90977 rev #26581 with authorization branch #90980
    
    
    Modified Files:
    ---------------
    	M       osprey/lai/cg_automod.cxx
    	M       osprey/targinfo/arm/cg/targ_ebo.cxx
    	M       osprey/targinfo/stxp70/cg/targ_ebo.cxx
    
    
    Diff command:
    -------------------------
    "svn diff --old=https://codex.cro.st.com/svnroot/open64/trunk@26579 --new=https://codex.cro.st.com/svnroot/open64/branches/fixes/codex-90977@26581 --diff-cmd tkdiff" 
    
    
    Merge command:
    -------------------------
    "svn merge https://codex.cro.st.com/svnroot/open64/trunk@26579 https://codex.cro.st.com/svnroot/open64/branches/fixes/codex-90977@26581" 
    
    Diff links to modified files:
    -----------------------------
    osprey/lai/cg_automod.cxx	http://codex.cro.st.com/svn/viewvc.php/trunk/osprey/lai/cg_automod.cxx?p2=branches/fixes/codex-90977/osprey/lai/cg_automod.cxx&r1=26579&r2=26581&roottype=svn&root=open64&diff_format=h
    osprey/targinfo/arm/cg/targ_ebo.cxx	http://codex.cro.st.com/svn/viewvc.php/trunk/osprey/targinfo/arm/cg/targ_ebo.cxx?p2=branches/fixes/codex-90977/osprey/targinfo/arm/cg/targ_ebo.cxx&r1=26579&r2=26581&roottype=svn&root=open64&diff_format=h
    osprey/targinfo/stxp70/cg/targ_ebo.cxx	http://codex.cro.st.com/svn/viewvc.php/trunk/osprey/targinfo/stxp70/cg/targ_ebo.cxx?p2=branches/fixes/codex-90977/osprey/targinfo/stxp70/cg/targ_ebo.cxx&r1=26579&r2=26581&roottype=svn&root=open64&diff_format=h
    
    
    
    
    git-svn-id: https://codex.cro.st.com/svnroot/open64/trunk@26583 9f6548e3-d00b-0410-a34f-e49d3b37daf0

--- osprey/lai/cg_automod.cxx
+++ osprey/lai/cg_automod.cxx
@@ -1243,6 +1243,11 @@ static BOOL BaseOffset_Combine(OP *mem_op, OP *inc_op, OPS *ops) {
   OP *new_mem_op = Dup_OP(mem_op);
   OP_Change_Opcode(new_mem_op, top_baseoffset);
   Copy_WN_For_Memory_OP(new_mem_op, mem_op);
+  // FdF 20100210: Unrolling information must be copied, it is used
+  // for memory dependence analysis
+  Set_OP_unrolling(new_mem_op, OP_unrolling(mem_op));
+  Set_OP_orig_idx(new_mem_op, OP_map_idx(mem_op));
+  Set_OP_unroll_bb(new_mem_op, OP_unroll_bb(mem_op));
   Set_OP_opnd(new_mem_op, OP_find_opnd_use(mem_op, OU_offset), baseoffset_tn);
   Set_OP_opnd(new_mem_op, OP_find_opnd_use(mem_op, OU_base), tn1);
   OPS_Append_Op(ops, new_mem_op);
